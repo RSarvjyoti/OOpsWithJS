dad2f1aad40feb94eae2a56b9d45a29066f231e9
RSarvjyoti <sarvjyoti64@gmail.com>
2024-12-05 13:54:39 +0530

    Inheritance and Encapsulation (getter, setter)

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # OOpsWithJS
+## OOps : -
+- Inheritance
+  1. Multiple Inheritance (not possible)
+  2. Multilevel Inheritance
+- Encapsulation
+- Abtraction
+- Polymorphism
---
+++ main.js
@@ -0,0 +1,53 @@
+// Syntax for the creating class
+class Rectangle {
+    constructor(length, width){
+        this.length = length;
+        this.width = width;
+    }
+
+    area = () => {
+        return this.length * this.width;
+    }
+}
+
+let myRectangle = new Rectangle(10, 10);
+console.log(myRectangle.area());
+
+class Car {
+    constructor(color, brand, speed) {
+        this.color = color;
+        this.brand = brand;
+        this.speed = speed;
+    }
+}
+// Inherit of Car property
+class SUV extends Car{
+    constructor(color, brand, speed) {
+        super(color, brand, speed);
+    }
+}
+
+let SUV1 = new SUV("red", "ABC", "100KMPH");
+console.log(SUV1);
+
+// Encapsulation
+
+class Student {
+    #score  // score make the private
+    constructor(name, score) {
+        this.name = name;
+        this.#score = score;
+    }
+
+    get marks() { // allowed the access private property
+        return this.#score
+    }
+    set marks(newVal) { // allowed the update private property
+        this.#score = newVal
+    }
+}
+
+let s1 = new Student("Sarvjyoti", 89);
+s1.score = 90 // We can not able to change the value of this property (It is a private)
+s1.marks = 90 // now able to update marks with help of setter function
+console.log(s1.marks)
